a7a9a8b1aadecff74a132069dfc9937922babc5c
Derwin <derwinprince61@gmail.com>
2024-08-05 14:47:48 -0400

    Baseline for BST remove

--- src/DataStructures/BinarySearchTree.hpp
+++ src/DataStructures/BinarySearchTree.hpp
@@ -12,7 +12,7 @@ T data;
 unique_ptr<BST_Node<T>> left;
 unique_ptr<BST_Node<T>> right;
 
-protected:
+public:
 unique_ptr<BST_Node<T>> GetLeft(){ return std::move(this->left); }
 unique_ptr<BST_Node<T>> GetRight(){ return std::move(this->right); }
 
@@ -25,10 +25,10 @@ T GetData() const{ return this->data; }
 
 void SetLeft(unique_ptr<BST_Node<T>> &left){ this->left = std::move(left); }
 void SetRight(unique_ptr<BST_Node<T>> &right){ this->right = std::move(right); }
+void ClearLeft(){ this->left = nullptr; }
 
 BST_Node<T>* GetLeftPtr(){ return this->left.get(); }
 BST_Node<T>* GetRightPtr(){ return this->right.get(); }
-
 };
 
 template<typename T>
@@ -41,8 +41,104 @@ BinarySearchTree() = default;
 ~BinarySearchTree() = default;
 void Insert(T data);
 void Remove(T data);
-
+unique_ptr<BST_Node<T>> GetSingleChild(BST_Node<T> *node);
+int CountChildren(BST_Node<T> *node);
+int WhichEdge(BST_Node<T> *node);
 };
+
+template<typename T>
+unique_ptr<BST_Node<T>> BinarySearchTree<T>::GetSingleChild(BST_Node<T> *node){
+  if(node->GetLeftPtr() != nullptr){ return node->GetLeft(); }
+  else{ return node->GetRight(); }
+}
+
+// Which child exists, 0 for left 1 for right
+template<typename T>
+int BinarySearchTree<T>::WhichEdge(BST_Node<T> *node){
+  if(node->GetLeftPtr() != nullptr){ return 0; }
+  else{ return 1; }
+}
+
+template<typename T>
+int BinarySearchTree<T>::CountChildren(BST_Node<T> *node){
+  int child_count = 0;
+  if(node->GetLeftPtr() != nullptr){ ++child_count; }
+  if(node->GetRightPtr() != nullptr){ ++child_count; }
+  return child_count;
+}
+
+template<typename T>
+void BinarySearchTree<T>::Remove(T data){
+
+  BST_Node<T>* current = this->root.get();
+  BST_Node<T>* previous = nullptr;
+
+  if(data == current->GetData()){ 
+    std::cout << "Root removal not implemented" << std::endl;
+    // this->root = current->GetLeft();
+    return;
+  }
+
+  int _switch = 0;
+  while(true){
+    if(data < current->GetData() || (_switch == 0 && data == current->GetData())){
+      if(data == current->GetData()){
+        int child_count = CountChildren(current);
+        switch(child_count)
+        {
+        case 0:{
+          previous->GetLeft().reset();
+          break;
+        }
+        case 1:{
+          unique_ptr<BST_Node<T>> child = GetSingleChild(current);
+          previous->SetLeft(child);
+          break;
+        }
+        case 2:{
+          break;
+        }
+        default:
+          std::cerr << "Child count out of range" << std::endl;
+          break;
+        }
+        return;
+      }
+      previous = current;
+      current = current->GetLeftPtr();
+      _switch = 0;
+    }
+
+    else if(data > current->GetData() || (_switch == 1 && data == current->GetData())){
+      if(data == current->GetData()){
+        int child_count = CountChildren(current);
+        switch(child_count)
+        {
+        case 0:{
+          previous->GetRight().reset();
+          break;
+        }
+        case 1:{
+          unique_ptr<BST_Node<T>> child = GetSingleChild(current);
+          previous->SetRight(child);
+          break;
+        }
+        case 2:{
+          break;
+        }
+        default:
+          std::cerr << "Child count out of range" << std::endl;
+          break;
+        }
+        return;
+      }
+      previous = current;
+      current = current->GetRightPtr();
+      _switch = 1;
+    }
+  }
+
+}
 
 template<typename T>
 void BinarySearchTree<T>::Insert(T data){
@@ -59,32 +155,21 @@ void BinarySearchTree<T>::Insert(T data){
   // If path node doesnt exist make new and set path
   // If path node does exist update to current and cmp value again
   */
-
+ 
   while(true){
     if(data < current->GetData()){
       if(current->GetLeftPtr() == nullptr){
         unique_ptr<BST_Node<T>> node = make_unique<BST_Node<T>>(data);
         current->SetLeft(node);
         return;
-      } else{
-        current = current->GetLeftPtr();
-      }
+      } else{ current = current->GetLeftPtr(); }
     } 
     else if(data > current->GetData()){
       if(current->GetRightPtr() == nullptr){
         unique_ptr<BST_Node<T>> node = make_unique<BST_Node<T>>(data);
         current->SetRight(node);
         return;
-      } else{
-        current = current->GetRightPtr();
-      }
+      } else{ current = current->GetRightPtr(); }
     }
   }
-
-}
-
-template<typename T>
-void BinarySearchTree<T>::Remove(T data){
-  std::cout << this->root.get()->GetData() << std::endl;
-
 }
--- src/main.cpp
+++ src/main.cpp
@@ -8,11 +8,17 @@ int main(){
   BinarySearchTree<int> tree;
   tree.Insert(45);
   tree.Insert(40);
-  tree.Insert(35);
   tree.Insert(55);
-  tree.Insert(48);
+  tree.Insert(42);
+  tree.Insert(44);
   tree.Insert(50);
-  tree.Remove(45);
+  tree.Insert(57);
+  tree.Insert(48);
+
+  tree.Remove(40);
+  tree.Remove(42);
+  tree.Remove(48);
+  tree.Remove(57);
 
   std::cout << "Goodbye" << std::endl;
 
