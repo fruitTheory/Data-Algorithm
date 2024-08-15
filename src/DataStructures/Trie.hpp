b2b655cbae2ae40d39d0a0234f5f47c511866b76
Derwin <derwinprince61@gmail.com>
2024-08-15 00:00:15 -0400

    Trie IP

--- src/DataStructures/Trie.hpp
+++ src/DataStructures/Trie.hpp
@@ -3,11 +3,13 @@
 #include <iostream>
 #include <unordered_map>
 #include <memory>
+#include <vector>
 
 using std::string;
 using std::unordered_map;
 using std::unique_ptr;
 using std::make_unique;
+using std::vector;
 
 class TrieNode{
 private:
@@ -21,8 +23,10 @@ void SetProperty(char key, unique_ptr<TrieNode> node){ child_property.emplace(ke
 TrieNode* GetChild(char key){ return child_property[key].get(); }
 size_t GetSize(){ return child_property.size(); }
 void DeleteNode(char key){ child_property.erase(key); }
-bool KeyExists(char key){ int x = child_property.count(key); return x > 0 ? true:false;}
-char GetChildCharAdv(char c);
+bool KeyExists(char key){ int x = child_property.count(key); return x > 0 ? true:false; }
+// char GetChildCharAdv(char c);
+char GetKey(){ return child_property.size()>0 ? child_property.begin()->first : ' '; }
+vector<char> GetKeys();
 };
 
 class Trie{
@@ -31,6 +35,8 @@ unique_ptr<TrieNode> root;
 
 protected:
 bool hasPrefix(string word);
+string SeekPrefixWords(char key, TrieNode* current);
+void PrintPrefixes(vector<string> words);
 void Lowercase(string &word){ for(char& c:word){c = tolower(c); }}
 
 public:
@@ -99,13 +105,74 @@ void Trie::DeleteWord(string word){
   }
 }
 
-char TrieNode::GetChildCharAdv(char c){ 
+// Return a vector of keys from current node
+vector<char> TrieNode::GetKeys(){ 
+  vector<char> keys;
   for(int x = 0; x < child_property.size(); x++){
     auto it = child_property.begin();
     std::advance(it, x);
     char current = it->first;
-    if(current == c){ return current; }
-  } return ' ';
+    keys.emplace_back(current);
+  } return keys;
+}
+
+string Trie::SeekPrefixWords(char key, TrieNode* current){
+
+  TrieNode* last_prefixed{nullptr};
+  int iter_prefix{0};
+  string stem;
+  stem.push_back(key);
+  int word_amount{0};
+
+  // Get amount of words from prefix
+  // while(current != nullptr){
+  //   if(current->isEndOfWord()){
+  //     ++word_amount;
+  //   }
+  //   current = current->GetChild(key);
+  // }
+
+  // Get letters from each word
+  while(current != nullptr){
+    if(current->GetSize() == 1){
+      key = current->GetKey();
+      stem.push_back(key);
+    }
+    std::cout << "End: " << key << " " << current->isEndOfWord() << std::endl;
+    current = current->GetChild(key);
+  }
+
+  for(char letter: stem){
+    std::cout << letter;
+  } print("");
+
+  return stem;
+}
+
+// Return vector of words that match prefix
+void Trie::MatchPrefix(string prefix){
+  Lowercase(prefix);
+  TrieNode* current = this->root.get();
+
+  // Set to node at prefix
+  for(size_t x = 0; x < prefix.size(); x++){
+    current = current->GetChild(prefix[x]); }
+
+  // Get keys at end of prefix
+  vector<char> keys = current->GetKeys();
+
+  vector<string> temp;
+
+  // For each key seek possible words
+  for(char key : keys){
+    std::cout << "key: " << key << std::endl;
+    string stem = SeekPrefixWords(key, current);
+    temp.push_back(prefix + stem);
+    break;
+  }
+
+  print(temp[0]);
+
 }
 
 bool Trie::hasPrefix(string word){
@@ -127,3 +194,12 @@ bool Trie::hasPrefix(string word){
 void Trie::Print(string c){
   Lowercase(c);
 }
+
+// char TrieNode::GetChildCharAdv(char c){ 
+//   for(int x = 0; x < child_property.size(); x++){
+//     auto it = child_property.begin();
+//     std::advance(it, x);
+//     char current = it->first;
+//     if(current == c){ return current; }
+//   } return ' ';
+// }
--- src/main.cpp
+++ src/main.cpp
@@ -6,12 +6,17 @@ int main(){
   std::cout << "Hello" << std::endl;
 
   Trie trie;
+  trie.Insert("Wordless");
+  // std::cout << trie.FindWord("Wordless") << std::endl;
   trie.Insert("Word");
   trie.Insert("Wont");
   trie.Insert("Test");
-  std::cout << trie.FindWord("Wont") << std::endl;
-  trie.DeleteWord("Wont");
-  std::cout << trie.FindWord("Wont") << std::endl;
+
+  trie.MatchPrefix("Wo");
+
+  // std::cout << trie.FindWord("Wont") << std::endl;
+  // trie.DeleteWord("Wont");
+  // std::cout << trie.FindWord("Wont") << std::endl;
 
   std::cout << "Goodbye" << std::endl;
 
